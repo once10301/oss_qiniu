cf18fef78897eaf6645fe5b7b393fa870924d9c5
once10301 <once10301@163.com>
2019-11-06 19:46:53 +0800

    .

--- ios/Classes/OssQiniuPlugin.m
+++ ios/Classes/OssQiniuPlugin.m
@@ -1,20 +1,36 @@
 #import "OssQiniuPlugin.h"
+#import <QiniuSDK.h>
+
 
 @implementation OssQiniuPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  FlutterMethodChannel* channel = [FlutterMethodChannel
-      methodChannelWithName:@"oss_qiniu"
-            binaryMessenger:[registrar messenger]];
-  OssQiniuPlugin* instance = [[OssQiniuPlugin alloc] init];
-  [registrar addMethodCallDelegate:instance channel:channel];
+    FlutterMethodChannel* channel = [FlutterMethodChannel
+                                     methodChannelWithName:@"oss_qiniu"
+                                     binaryMessenger:[registrar messenger]];
+    OssQiniuPlugin* instance = [[OssQiniuPlugin alloc] init];
+    [registrar addMethodCallDelegate:instance channel:channel];
 }
 
 - (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
-  if ([@"getPlatformVersion" isEqualToString:call.method]) {
-    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
-  } else {
-    result(FlutterMethodNotImplemented);
-  }
+    if ([@"upload" isEqualToString:call.method]) {
+        NSDictionary *argsMap = call.arguments;
+        NSString *filePath = argsMap[@"path"];
+        NSString *token = argsMap[@"token"];
+        QNConfiguration *config =[QNConfiguration build:^(QNConfigurationBuilder *builder) {
+            NSMutableArray *array = [[NSMutableArray alloc] init];
+        }];
+        QNUploadManager *upManager = [[QNUploadManager alloc] initWithConfiguration:config];
+        [upManager putFile:filePath key:nil token:token complete:^(QNResponseInfo *info, NSString *key, NSDictionary *resp) {
+            if(info.ok) {
+                result(@"成功");
+            } else {
+                result(@"失败");
+            }
+            NSLog(@"info ===== %@", info);
+            NSLog(@"resp ===== %@", resp);
+        } option:nil];
+    } else {
+        result(FlutterMethodNotImplemented);
+    }
 }
-
 @end
--- lib/oss_qiniu.dart
+++ lib/oss_qiniu.dart
@@ -3,11 +3,9 @@ import 'dart:async';
 import 'package:flutter/services.dart';
 
 class OssQiniu {
-  static const MethodChannel _channel =
-      const MethodChannel('oss_qiniu');
+  static const MethodChannel _channel = const MethodChannel('oss_qiniu');
 
-  static Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
+  static Future<String> upload(String path, String token) async {
+    return await _channel.invokeMethod('upload', {'path': path, 'token': token});
   }
 }
