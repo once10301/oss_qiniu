f3be0caa34687f7c17ab5fa62adcd9faf6a17fe4
once10301 <175915345@qq.com>
2020-12-23 10:24:55 +0800

    .

--- android/src/main/java/com/xiyou/oss_qiniu/OssQiniuPlugin.java
+++ android/src/main/java/com/xiyou/oss_qiniu/OssQiniuPlugin.java
@@ -1,11 +1,16 @@
 package com.xiyou.oss_qiniu;
 
+import android.app.Activity;
+
 import com.qiniu.android.http.ResponseInfo;
 import com.qiniu.android.storage.UpCompletionHandler;
 import com.qiniu.android.storage.UploadManager;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
@@ -17,12 +22,14 @@ import io.flutter.plugin.common.PluginRegistry.Registrar;
  * OssQiniuPlugin
  */
 public class OssQiniuPlugin implements MethodCallHandler {
+    private static Activity activity;
     private MethodChannel.Result result;
 
     /**
      * Plugin registration.
      */
     public static void registerWith(Registrar registrar) {
+        activity = registrar.activity();
         final MethodChannel channel = new MethodChannel(registrar.messenger(), "oss_qiniu");
         channel.setMethodCallHandler(new OssQiniuPlugin());
     }
@@ -44,17 +51,33 @@ public class OssQiniuPlugin implements MethodCallHandler {
         UploadManager uploadManager = new UploadManager();
         uploadManager.put(path, key, token, new UpCompletionHandler() {
             @Override
-            public void complete(String key, ResponseInfo info, JSONObject res) {
+            public void complete(String key, final ResponseInfo info, final JSONObject res) {
                 //res包含hash、key等信息，具体字段取决于上传策略的设置
                 if (info.isOK()) {
-                    try {
-                        result.success(res.get("key"));
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                        result.success("");
-                    }
+                    activity.runOnUiThread(new Runnable() {
+                        public void run() {
+                            try {
+                                Map<String, Object> map = new HashMap<>();
+                                map.put("success", 0);
+                                map.put("result", res.get("key"));
+                                result.success(map);
+                            } catch (JSONException e) {
+                                Map<String, Object> map = new HashMap<>();
+                                map.put("success", 1);
+                                map.put("result", e.getMessage());
+                                result.success(map);
+                            }
+                        }
+                    });
                 } else {
-                    result.success("");
+                    activity.runOnUiThread(new Runnable() {
+                        public void run() {
+                            Map<String, Object> map = new HashMap<>();
+                            map.put("success", 1);
+                            map.put("result", info.error);
+                            result.success(map);
+                        }
+                    });
                 }
             }
         }, null);
